5001c93d53dd8278bb71e19126134894c407cd09
Aaron Evans <aaron.evans@cgifederal.com>
2015-10-14 18:02:10 -0400

    Stop tracking production.js and staging.js already wasn't tracking local.js

--- config/env/staging.js
+++
@@ -1,9 +0,0 @@
-var mongoDBurl = '';
-module.exports = {
-    AGOLadminCredentials:
-    {username: "",password: ""},
-    sessionOptions: {secret:""},
-//touchAfter: 24*3600 means only update session one time in 24 hrs instead of every refresh (unless session data is changed of course)
-    mongoStoreOption: {url: mongoDBurl},
-    mongoDBurl: mongoDBurl
-};
