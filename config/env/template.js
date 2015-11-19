343ee9975b81b53b7ea71100b95bc14012330b5d
Aaron Evans <aaron.evans@cgifederal.com>
2015-11-19 15:45:35 -0500

    Load first page initially and then load remaining pages in background

--- config/env/template.js
+++ config/env/template.js
@@ -1,10 +1,12 @@
-var mongoDBurl='';
+//Enter the url of the mongo DB 
+var mongoDBurl='mongodb://localhost:27017/egam';
 module.exports = {
     AGOLorgID: "cJ9YHowT8TU7DUyn",
     AGOLadminCredentials:
     {username: "",password: "",appID:"",appSecretDisable:"",expiration:1440},
     portal: "https://epa.maps.arcgis.com",
 //saveUninitialized:false,resave:false limits resaving of session data that is unchanged or uninitialized
+//Enter any string for secret, something somebody would never guess
     sessionOptions: {secret:"",saveUninitialized:false,resave:false},
 //touchAfter: 24*3600 means only update session one time in 24 hrs instead of every refresh (unless session data is changed of course)
     mongoStoreOption: {url: mongoDBurl,touchAfter: 24 * 3600},
@@ -17,5 +19,6 @@ module.exports = {
     {smtp:{host:"smtp.rtpnc.epa.gov.disable",port:25,service:null,user: "",password: ""}
       ,admins: "aaron.evans@cgi.com;brett.gaines@cgi.com;dyarnell@innovateteam.com;Hultgren.Torrin@epa.gov",defaultFrom: "egam@epa.gov"
       ,disabled: true},
-    superUserGroup: ""
+    superUserGroup: "",
+    maxRowLimit:null
 };
