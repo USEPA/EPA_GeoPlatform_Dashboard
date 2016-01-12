f14b47fcacd3706a0f1a3eac72c55a55d0fbbea4
bgaines <Brett.Gaines@cgifederal.com>
2016-01-12 10:21:56 -0600

    Add custom build of Esri JS API, built with Esri JSO with v3.15

---
+++ public/js/esrijsapi/esri/tasks/ParameterValue.js
@@ -0,0 +1,2 @@
+//>>built
+define("esri/tasks/ParameterValue",["dojo/_base/declare","dojo/_base/lang","dojo/has","../kernel"],function(a,b,c,d){a=a(null,{declaredClass:"esri.tasks.ParameterValue",constructor:function(a){b.mixin(this,a)}});c("extend-esri")&&b.setObject("tasks.ParameterValue",a,d);return a});
