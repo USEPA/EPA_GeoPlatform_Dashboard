fb60fbceebfb5cbacc89ccda50298f6333753012
bgaines <Brett.Gaines@cgifederal.com>
2016-05-09 12:02:36 -0400

    Add mocha tests for log in procedures and cookies WIP

--- test/test.js
+++ test/test.js
@@ -24,3 +24,55 @@ describe('Root URL test',function(){
     });
   });
 });
+
+
+describe('Anonymous (unauthenticated) Request to API',function(){
+  it('should return an error that user must be logged in' ,function(done){
+    request(app)
+      .get('/gpdashboard/gpousers/list')
+      .end(function(err,res){
+        if (err) {
+          return done(err);
+        }
+        res.status.should.equal(200);
+        //console.log(res.body.errors[0].code);
+        res.body.errors[0].code.should.equal('LoginRequired');
+        done();
+    });
+  });
+});
+
+describe('Authorization Mechanism Testing Suite', function () {
+
+  it('should create a user session for a valid GPO (non-SSO) user', function (done) {
+    request(app)
+      .post('/gpdashboard/login')
+      .set('Accept','*/*')
+      .send({'username': 'Gaines.Brett_EPA', 'token': 'rKvkMypYiQi4K6gN2-MVhzXolK149e_Mne4Fl2QotjKSqDY9UAdVpaPYOoBVKmrxk3KjuBgIvLWlHjAVgFJMllEvQhLfwVkdElKII4C9pXNToDbly0CA87JMKiuOGV8uEnXzTlDwVL2PWdyRqGEOvhjJSt0DqxdbisVLiL44R4LlrvRa_cUqjwZpwRbi7mAf6iDIIzRjXxf4FFW3x6rN0Q..'})
+      .expect(200)
+      .end(function (err, res) {
+        console.log(res);
+        res.body.id.should.equal('1');
+        res.body.short_name.should.equal('Gaines.Brett_EPA');
+        res.body.email.should.equal('gaines.brett@epa.gov');
+        // Save the cookie to use it later to retrieve the session
+        cookies = res.headers['set-cookie'].pop().split(';')[0];
+        done();
+      });
+  });
+  
+  it('should get the user session for current user', function (done) {
+    var req = request(app).get('/gpdashboard/login');
+    // Set cookie to get saved user session
+    req.cookies = cookies;
+    req.set('Accept','application/json')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .end(function (err, res) {
+        res.body.id.should.equal('1');
+        res.body.short_name.should.equal('Gaines.Brett_EPA');
+        res.body.email.should.equal('gaines.brett@epa.gov');
+        done();
+      });
+  });
+});
