de6cca1b23a2479f04aa57c3e9bca5103e5a312c
bryan.chastain <bryan.chastain@cgifederal.com>
2016-05-18 09:53:13 -0500

    beautifying, first pass

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,28 @@
-module.exports = function (app) {
+module.exports = function(app) {
   var express = require('express');
   var router = express.Router();
   var request = require('request');
 
-  router.use('/login', function (req, res, next) {
+  router.use('/login', function(req, res, next) {
     var utilities = require(app.get('appRoot') + '/shared/utilities');
     var config = app.get('config');
 
-    //Use community/self to get user info including the group info portals/self gets user as part of portal info but no groups
+    //Use community/self to get user info including the group info portals/self
+    //gets user as part of portal info but no groups
     var url = config.portal + '/sharing/rest/community/self';
 
     var errors = [];
     var inputs = utilities.getCleanRequestInputs(req, errors);
 
-    if (errors.length > 0) return res.json(utilities.getHandleError({},"DirtyRequestInput")(errors.join("\n")));
+    if (errors.length > 0) {
+      return res.json(
+          utilities.getHandleError({},'DirtyRequestInput')(errors.join('\n')));
+    }
 
     var token = inputs.token;
     var username = inputs.username;
 
-    var qsPars = {'token': token, 'f': 'json'};
+    var qsPars = {token: token, f: 'json'};
 
     //Pass parameters via form attribute
     var requestPars = {method: 'get', url: url, qs: qsPars};
@@ -26,14 +30,15 @@ module.exports = function (app) {
     var hrClass = require(app.get('appRoot') + '/shared/HandleGPOresponses');
     var hr = new hrClass(config);
 
-    //define this and it will be filled in handleResponse or handleError
+    //Define this and it will be filled in handleResponse or handleError
     var resObject = {};
 
-    console.log("LOGIN session.username: " + req.session.username)
+    console.log('LOGIN session.username: ' + req.session.username);
 
-    if ('session' in req && req.session.username && username === req.session.username) {
+    if ('session' in req && req.session.username &&
+        username === req.session.username) {
       //If they are already logged into the session don't do it all over again
-      console.log("USER ALREADY IN SESSION");
+      console.log('USER ALREADY IN SESSION');
       res.json({errors: [], body: {user: req.session.user}});
     } else {
       hr.callAGOL(requestPars)
@@ -41,9 +46,9 @@ module.exports = function (app) {
         .then(getSuperUser)
         .then(getAuthGroupsAndOwnerIDs)
         .then(updateDBonLogin)
-        .catch(utilities.getHandleError(resObject, "LoginError"))
+        .catch(utilities.getHandleError(resObject, 'LoginError'))
         //Now that update is done we can finally return result
-        .done(function () {
+        .done(function() {
           res.json(resObject)
         });
     }
@@ -51,22 +56,25 @@ module.exports = function (app) {
     function handleResponse(body) {
       var user = hr.current;
       if (username === user.username) {
-        //save the user to the session
+        //Save the user to the session
         if ('session' in req) {
           req.session.username = username;
           req.session.token = token;
           req.session.user = user;
         }
-        resObject = {errors:[], body: {user: user}};
+        resObject = {errors: [], body: {user: user}};
       } else {
-        utilities.getHandleError(resObject,"UsernameTokenMismatch")("Username does not match token");
+        utilities.getHandleError(resObject,
+            'UsernameTokenMismatch')('Username does not match token');
       }
       return resObject;
     }
 
     function getAuthGroupsAndOwnerIDs() {
       //If there was an error with resObject then don't need to do this
-      if (resObject.errors.length > 0) return false;
+      if (resObject.errors.length > 0) {
+        return false;
+      }
 
       var user = req.session.user;
 
@@ -78,30 +86,39 @@ module.exports = function (app) {
       var monk = app.get('monk');
       var usersCollection = monk.get('GPOusers');
       var ownerIDsCollection = monk.get('GPOownerIDs');
-      //Set up classes used to find OwnerIDs. Can be reused across app because only saved data is list of available
-      //Auth Groups in system
-      var UpdateAuthGroupsAndOwnerIDsClass = require(appRoot + '/shared/UpdateAuthGroupsAndOwnerIDs');
-      var updateAuthGroupsAndOwnerIDs = new UpdateAuthGroupsAndOwnerIDsClass(usersCollection, ownerIDsCollection, authgroupsCollection);
+      //Set up classes used to find OwnerIDs. Can be reused across app because
+      //only saved data is list of available Auth Groups in system
+      var UpdateAuthGroupsAndOwnerIDsClass = require(appRoot +
+          '/shared/UpdateAuthGroupsAndOwnerIDs');
+      var updateAuthGroupsAndOwnerIDs = new UpdateAuthGroupsAndOwnerIDsClass(
+          usersCollection, ownerIDsCollection, authgroupsCollection);
 
-      //First have to update any new auth groups they might have been added to then get ownerIDs for each auth group
+      //First have to update any new auth groups they might have been added to
+      //then get ownerIDs for each auth group
       return updateAuthGroupsAndOwnerIDs.updateAuthGroups(user)
-        .then(function (updateUser) {
+        .then(function(updateUser) {
           user = updateUser;
-          return updateAuthGroupsAndOwnerIDs.getOwnerIDsForEachAuthGroup(user)
+          //Pass empty object user.authGroupsByownerID and it will be generated
+          //also while getting OwnerIDsForEachAuthGroup
+          user.authGroupsByownerID = {};
+          return updateAuthGroupsAndOwnerIDs
+            .getOwnerIDsForEachAuthGroup(user,user.authGroupsByownerID);
         })
-        //Now that authgroups with corresponding ownerIDs is returned we can save them in Session
-        .then(function (ownerIDsByAuthGroup) {
-          user.ownerIDsByAuthGroup=ownerIDsByAuthGroup;
+        //Now that authgroups with corresponding ownerIDs is returned we can
+        //save them in Session
+        .then(function(ownerIDsByAuthGroup) {
+          user.ownerIDsByAuthGroup = ownerIDsByAuthGroup;
           req.session.user = user;
           resObject.body.user = user;
         })
-        //Now get all of the ownerIDs this user can see based on their auth group membership (if they are an admin)
+        //Now get all of the ownerIDs this user can see based on their auth
+        //group membership (if they are an admin)
         //If user not an admin, then they just see their own stuff
-        .then(function () {
-            return updateAuthGroupsAndOwnerIDs.getOwnerIDs(user);
+        .then(function() {
+          return updateAuthGroupsAndOwnerIDs.getOwnerIDs(user);
         })
         //Now that ownerIDs is returned we can save them in Session
-        .then(function (ownerIDs) {
+        .then(function(ownerIDs) {
           //Note: Super user will see all ownerIDs
           req.session.ownerIDs = ownerIDs;
           return resObject;
@@ -113,19 +130,25 @@ module.exports = function (app) {
 
     function getSuperUser() {
       //If there was an error with resObject then don't need to do this
-      if (resObject.errors.length > 0) return false;
+      if (resObject.errors.length > 0) {
+        return false;
+      }
 
       var Q = require('q');
       var user = req.session.user;
       var config = app.get('config');
-      if (!config.superUserGroup || !user.groups) return false;
+      if (!config.superUserGroup || !user.groups) {
+        return false;
+      }
 
-      //.some() breaks and returns true when callback returns true. If all are false .some() returns false.
-      var isSuperUser = user.groups.some(function (group) {
+      //.some() breaks and returns true when callback returns true.
+      //If all are false .some() returns false.
+      var isSuperUser = user.groups.some(function(group) {
         return (group.title === config.superUserGroup);
       });
 
-      //if not super user return false. otherwise have to find all owner IDs in GPOitems if user is Super User
+      //If not super user return false. otherwise have to find all owner IDs in
+      //GPOitems if user is Super User
       req.session.user.isSuperUser = isSuperUser;
 
       return isSuperUser;
@@ -133,64 +156,79 @@ module.exports = function (app) {
 
     function updateDBonLogin() {
       //If there was an error with resObject then don't need to do this
-      if (resObject.error !== null) return true;
+      if (resObject.error !== null) {
+        return true;
+      }
 
       var Q = require('q');
       var appRoot = app.get('appRoot');
       var config = app.get('config');
 
-      var DownloadGPOdataClass = require(appRoot + '/shared/DownloadGPOdata')
+      var DownloadGPOdataClass = require(appRoot + '/shared/DownloadGPOdata');
       downloadGPOdata = new DownloadGPOdataClass();
 
-      //This is the number of items downloaded from AGOL in one request (max is 100)
+      //This is the number of items downloaded from AGOL in one request
+      //(max is 100)
       downloadGPOdata.requestItemCount = 100;
 
-      //For some reason when doing Modified Only (dontRemoveGPOitems = true) if AsyncRequestLimit is large response results were mysteriously dropping
-      //I think it is because doing Modified Only takes MUCH longer (order of 5000 ms) while getting all is faster (order of 500ms)
-      //Note: This only occurs when there are A LOT of total rows in the Modified Date Range of query. Something to do with paging slowing down maybe....
-      //But usually finding Modified Only doesn't entail retrieving a lot of rows so for updating when logging in should get modified Only (dontRemoveGPOitems =true)
-      //Set AsyncRequestLimit=5 just to be safe but probably will never even have this many modified (500) in one day
+      //For some reason when doing Modified Only (dontRemoveGPOitems = true)
+      //if AsyncRequestLimit is large response results were mysteriously
+      //dropping. I think it is because doing Modified Only takes MUCH longer
+      //(order of 5000 ms) while getting all is faster (order of 500ms)
+      //Note: This only occurs when there are A LOT of total rows in the
+      //Modified Date Range of query. Something to do with paging slowing down
+      //maybe.... But usually finding Modified Only doesn't entail retrieving a
+      //lot of rows so for updating when logging in should get modified Only
+      //(dontRemoveGPOitems =true). Set AsyncRequestLimit=5 just to be safe but
+      //probably will never even have this many modified (500) in one day
       downloadGPOdata.AsyncRequestLimit = 5;
       downloadGPOdata.AsyncRowLimit = 25;
       downloadGPOdata.AsyncAuditRowLimit = 100;
 
-      //Only get the metadata because getting slash data is much slower, due to more data over the wire
+      //Only get the metadata because getting slash data is much slower, due to
+      //more data over the wire
       downloadGPOdata.onlyGetMetaData = true;
-      //Don't remove the gpo items because then we would have to query ENTIRE DB from AGOL to find which were removed on AGOL
+      //Don't remove the gpo items because then we would have to query ENTIRE DB
+      //from AGOL to find which were removed on AGOL
       downloadGPOdata.dontRemoveGPOitems = true;
 
-      //set in token
+      //Set in token
       downloadGPOdata.token = req.session.token;
       downloadGPOdata.portal = config.portal;
       downloadGPOdata.mongoDBurl = config.mongoDBurl;
       downloadGPOdata.orgID = config.AGOLorgID;
       downloadGPOdata.ownerIDs = req.session.ownerIDs;
-      console.log("downloadGPOdata.ownerIDs : " + downloadGPOdata.ownerIDs);
+      console.log('downloadGPOdata.ownerIDs : ' + downloadGPOdata.ownerIDs);
 
-      //The download process will be passed to a queue so that 2 users are not updating same stuff possibly
+      //The download process will be passed to a queue so that 2 users are not
+      //updating same stuff possibly
       //After download is complete the response body of login will be sent back
-      //When the download->respond promise chain is done, the queue will move on to next task in queue
+      //When the download->respond promise chain is done, the queue will move on
+      //to next task in queue
       var defer = Q.defer();
 
       //Each task will be for a single user name.
-      //For Admin, multiple user names will be run thus Admin download can not be done till all usernames in ownerIDs
-      //are available
-      //groupedTasks is a list of all tasks that must be free for this Admin until downloadGPOdata.download can be
-      //executed
+      //For Admin, multiple user names will be run thus Admin download can not
+      //be done till all usernames in ownerIDs are available
+      //groupedTasks is a list of all tasks that must be free for this Admin
+      //until downloadGPOdata.download can be executed
       var TaskReady = {};
 
       var tasksQueues = app.get('tasksQueues');
       var taskBase = 'updateDBonLogin';
 
-      //Only if list of owner IDs is available do we need to add ownerID to task names
-      if (Array.isArray(downloadGPOdata.ownerIDs) && downloadGPOdata.ownerIDs.length > 0) {
-        //task name will be updateDBonLogin-ownerID
-        downloadGPOdata.ownerIDs.forEach(function (ownerID) {
+      //Only if list of owner IDs is available do we need to add ownerID to
+      //task names
+      if (Array.isArray(downloadGPOdata.ownerIDs) &&
+          downloadGPOdata.ownerIDs.length > 0) {
+        //Task name will be updateDBonLogin-ownerID
+        downloadGPOdata.ownerIDs.forEach(function(ownerID) {
           TaskReady[taskBase + '-' + ownerID] = false;
         });
 
-        Object.keys(TaskReady).forEach(function (task) {
-          //The actual download process "updateDBonLoginPromise" with "data" as argument will be called when it is next in queue
+        Object.keys(TaskReady).forEach(function(task) {
+          //The actual download process "updateDBonLoginPromise" with "data" as
+          //argument will be called when it is next in queue
           var data = {task: task, TaskReady: TaskReady, defer: defer};
           tasksQueues.add(task, updateDBonLoginPromise, data);
         });
@@ -202,10 +240,10 @@ module.exports = function (app) {
     }
 
     //This will be started by task queue when tasks ahead of it are done
-    var updateDBonLoginPromise = function (data) {
+    var updateDBonLoginPromise = function(data) {
       //Check the task as ready then determine if all are ready
       data.TaskReady[data.task] = true;
-      var allReady = Object.keys(data.TaskReady).every(function (t) {
+      var allReady = Object.keys(data.TaskReady).every(function(t) {
         return data.TaskReady[t] === true
       });
 
@@ -213,20 +251,21 @@ module.exports = function (app) {
         return downloadGPOdata.download()
           //After download is done, the deferred is resolved.
           //Also when updateDBonLoginPromise is done the task in queue
-          .then(function () {
-            console.log("data.task downloaded: " + data.task);
+          .then(function() {
+            console.log('data.task downloaded: ' + data.task);
             data.defer.resolve();
           });
-      } else {
-        //If all tasks are not ready to be run because they haven't been freed from queue, send promise so Task=updateDBonLogin-ownerID will be held until download done
-        return data.defer.promise;
       }
+      //If all tasks are not ready to be run because they haven't been freed
+      //from queue, send promise so Task=updateDBonLogin-ownerID will be held
+      //until download done
+      return data.defer.promise;
     };
   });
 
-  router.use('/logout', function (req, res, next) {
+  router.use('/logout', function(req, res, next) {
     if ('session' in req) {
-      console.log("Logout for username: " + req.session.username);
+      console.log('Logout for username: ' + req.session.username);
       req.session.destroy();
     }
     res.json({errors: [], body: {}});
@@ -234,14 +273,14 @@ module.exports = function (app) {
 
   //Get shared/Audit.js from client http reference to Public folder
   //Not sure if this is going to work with caching etc
-  router.use('/js/Audit.js', function (req, res, next) {
+  router.use('/js/Audit.js', function(req, res, next) {
     var fs = require('fs');
     var filePath = app.get('appRoot') + '/shared/Audit.js';
     var stat = fs.statSync(filePath);
 
     res.writeHead(200, {
       'Content-Type': 'application/javascript',
-      'Content-Length': stat.size
+      'Content-Length': stat.size,
     });
 
     var readStream = fs.createReadStream(filePath);
--- shared/UpdateGPOuser.js
+++ shared/UpdateGPOuser.js
@@ -1,64 +1,92 @@
-var appRoot = appRoot=require('app-root-path');
-var utilities=require(appRoot + '/shared/utilities');
+var appRoot = appRoot = require('app-root-path');
+var utilities = require(appRoot + '/shared/utilities');
 
-var UpdateGPOgenericClass=require(appRoot + '/shared/UpdateGPOgeneric');
-//call parent constructor and then use child constructor stuff too
+var UpdateGPOgenericClass = require(appRoot + '/shared/UpdateGPOgeneric');
+//Call parent constructor and then use child constructor stuff too
 utilities.inheritClass(UpdateGPOgenericClass,UpdateGPOuser);
 
 //Name this function so that class name will come up in debugger, etc.
-function UpdateGPOuser(collection,extensionsCollection,session,config){
-//run the parent constructor first
-//"user" is the updateKey used for updating and "user" is just updateName which is text used on errors, logging, etc
-  this.__parent__.constructor.call(this,"username","user",collection,extensionsCollection,session,config);
+function UpdateGPOuser(collection,extensionsCollection,session,config) {
+  //Run the parent constructor first
+  //"user" is the updateKey used for updating and "user" is just updateName
+  //which is text used on errors, logging, etc
+  this.__parent__.constructor.call(
+      this,
+      'username',
+      'user',
+      collection,
+      extensionsCollection,
+      session,
+      config
+  );
 
-//Nothing extra in constructor for UpdateGPOuser
+  //Nothing extra in constructor for UpdateGPOuser
 
 }
 
 UpdateGPOuser.prototype.checkPermission = function() {
-//Check if logged in user can update this user
+  //Check if logged in user can update this user
   var self = this;
   var Q = require('q');
 
   var hasPermission = false;
-//user can see this username they can modify it
-  if (self.session.ownerIDs.indexOf(self.updateDoc.username)>=0) return Q.fcall(function () {return true});
-//If logged in user is not admin then can not modify external user
-  if (! self.session.user.isAdmin) return Q.fcall(function () {return false});
-//If external user and logged in user is admin then they can modifiy it
-  return Q(self.collection.findOne({username:self.updateDoc.username}, {isExternal:1}))
-    .then(function (doc) {
-//if username not found then return false (maybe have error later but should we expose this type of lookup?)
-      if (! doc) return false;
+  //User can see this username they can modify it
+  if (self.session.ownerIDs.indexOf(self.updateDoc.username) >= 0) {
+    return Q.fcall(function() {return true});
+  }
+  //If logged in user is not admin then can not modify external user
+  if (!self.session.user.isAdmin) {
+    return Q.fcall(function() {return false});
+  }
+  //If external user and logged in user is admin then they can modifiy it
+  return Q(self.collection.findOne({username: self.updateDoc.username},{isExternal: 1}))
+    .then(function(doc) {
+      //If username not found then return false (maybe have error later but
+      //should we expose this type of lookup?)
+      if (!doc) {
+        return false;
+      }
       return doc.isExternal;
     });
 };
 
-//In the future we might want to update user info and add the authgroup and will probably have to just over ride entire parent updateRemote function
+//In the future we might want to update user info and add the authgroup and will
+//probably have to just over ride entire parent updateRemote function
 UpdateGPOuser.prototype.getRemoteUpdateRequest = function(formData) {
   var self = this;
 
-//For now we are only adding the authGroup here
-  if (! self.updateDoc.authGroup) return null;
+  //For now we are only adding the authGroup here
+  if (!self.updateDoc.authGroup) {
+    return null;
+  }
 
-//Need to find the auth group ID that we are adding user to . using require reload so we don't have to restart EGAM if authGroup added can change later when stable
+  //Need to find the auth group ID that we are adding user to . using require
+  //reload so we don't have to restart EGAM if authGroup added can change later
+  //when stable
   var requireReload = require('require-reload')(require);
   var authGroupIDs = requireReload(self.appRoot + '/config/authGroups.js').ids;
-  if (! authGroupIDs[self.updateDoc.authGroup]) console.log("self.updateDoc.authGroup does not exist: " + JSON.stringify(self.updateDoc));
+  if (!authGroupIDs[self.updateDoc.authGroup]) {
+    console.log('self.updateDoc.authGroup does not exist: ' +
+        JSON.stringify(self.updateDoc));
+  }
   var authGroupID = authGroupIDs[self.updateDoc.authGroup].id;
-  if (! authGroupID) return null;
+  if (!authGroupID) {
+    return null;
+  }
 
-  var url= self.config.portal + '/sharing/rest/community/groups/' + authGroupID + '/addUsers';
+  var url = self.config.portal + '/sharing/rest/community/groups/' +
+      authGroupID + '/addUsers';
 
-//Just over ride the formData passed in here. All we will be updating is user to group
-  formData = {users:self.updateDoc.username};
+  //Just over ride the formData passed in here. All we will be updating is user
+  //to group
+  formData = {users: self.updateDoc.username};
 
-//      console.log(url);
-  var qs = {token: self.session.token,f:'json'};
-//      console.log(req.session.token);
+  //Console.log(url);
+  var qs = {token: self.session.token,f: 'json'};
+  //Console.log(req.session.token);
 
-//Pass parameters via form attribute
-  var requestPars = {method:'post', url:url, formData:formData, qs:qs };
+  //Pass parameters via form attribute
+  var requestPars = {method: 'post', url: url, formData: formData, qs: qs };
   return requestPars;
 };
 
