fe0c080824f6ff26c783a610390bc0632ed794b6
Evans, Aaron (CGI Federal) <aaron.evans@cgifederal.com>
2016-06-09 13:11:07 -0400

    changed the TableClass download timeout and let it be member on class. Also cleaned up some of the table stuff (eg. do the formatting of date using generalized function in ko html) added col filters to gpoUsers table.

--- public/gpdashboard/js/gpoItems.js
+++ public/gpdashboard/js/gpoItems.js
@@ -214,19 +214,6 @@ egam.models.gpoItems.RowModelClass = function(doc, index) {
     return this.doc.AuditData.compliant ? 'Pass' : 'Fail';
   }, this);
 
-  //Format Modified Date
-  this.modifiedDate = ko.computed(function() {
-    var monthNames = [
-      'Jan', 'Feb', 'Mar',
-      'Apr', 'May', 'Jun', 'Jul',
-      'Aug', 'Sep', 'Oct',
-      'Nov', 'Dec',
-    ];
-
-    var modDate = new Date(this.doc.modified);
-    return monthNames[modDate.getMonth()] + ' ' + modDate.getDate() + ', ' + modDate.getFullYear();
-  }, this);
-
 };
 
 //This is the FULL model which binds to the modal allowing 2 way data binding and updating etc
