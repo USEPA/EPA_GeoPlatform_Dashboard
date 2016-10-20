73ab930ebb9cd2ea5c56451f47c4d93ba5621209
Evans, Aaron (CGI Federal) <aaron.evans@cgifederal.com>
2016-10-20 12:53:47 -0400

    just a small fix to download script i found when trying to get slash data size from http HEAD request

--- scripts/downloadGPOheaders.js
+++ scripts/downloadGPOheaders.js
@@ -168,18 +168,16 @@ function getGPOheadersAsync() {
   console.log(hr.saved.currentGPOrow + ' ' + GPOids.length);
 
   async.forEachOf(GPOids, function(value, key, done) {
-    //      Console.log(key+hr.saved.currentGPOrow)
-    getSingleGPOheader(key + asyncStartCurrentGPOrow)
-        .then(function() {
-          //                Console.log(String(key+1) + 'done');
-        done();})
+      //      Console.log(key+hr.saved.currentGPOrow)
+      getSingleGPOheader(key + asyncStartCurrentGPOrow)
         .catch(function(err) {
-          console.error('single gpo header Error received:', err);
+          console.error('single gpo header Error received for id = ' + hr.saved.GPOids[currentGPOrow - 1] + ' : ' + err);
+          hr.saved.currentGPOrow += 1;
         })
         .done(function() {
-          //          Console.log('for loop success')
-        });
-  }
+          done();}
+        );
+    }
     , function(err) {
       if (err) console.error('for each error :' + err.message);
       //Resolve this promise
